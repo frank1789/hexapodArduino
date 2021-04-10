b1ffc5a1c24240e706e862a1d4109e3872d2d7b6
Francesco <francesco.argentieri89@gmail.com>
2021-04-10 19:27:39 +0200

    Add constants

---
+++ mainservo/constants.h
@@ -0,0 +1,18 @@
+#ifndef HEXAPOD_CONSTANTS_H
+#define HEXAPOD_CONSTANTS_H
+
+// numeric constants
+constexpr int kNumberLegs = 6;
+
+// components' name constants
+constexpr char kComponentLegCoxa[] = "coxa";
+constexpr char kComponentLegFemur[] = "femur";
+constexpr char kComponentLegTibia[] = "tibia";
+constexpr char kLeftFrontLeg[] = "leftfrontleg";
+constexpr char kLeftMidLeg[] = "leftmidleg";
+constexpr char kLeftBackLeg[] = "leftbackleg";
+constexpr char kRightFrontLeg[] = "rightfrontleg";
+constexpr char kRightMidLeg[] = "rightmidleg";
+constexpr char kRightBackLeg[] = "rightbackleg";
+
+#endif  // HEXAPOD_CONSTANTS_H
