ea72eff290c4fd2d768ebc3e542d59feb71f1994
Francesco Argentieri <francesco.argentieri89@gmail.com>
2021-04-11 08:30:58 +0200

    Add print in setup phase

--- mainservo/mainservo.ino
+++ mainservo/mainservo.ino
@@ -13,10 +13,19 @@ static const char *legs[] = {
 
 void setup() {
   Serial.begin(115200);
-  Serial.print("initialize legs connected to pin");
+  Serial.print("initialize legs connected to pin\n");
   auto *hxm = HexapodManger::getInstance();
   for (int i = 0; i < kPins; i=i+3) {
     int index = i / 3;
+    Serial.print("setup leg: ");
+    Serial.println(legs[i]);
+    Serial.println("assign pins: ");
+    Serial.println("coxa: ");
+    Serial.println(i+1);
+    Serial.println("femur: ");
+    Serial.println(i+2);
+    Serial.println("tibia: ");
+    Serial.println(i+3);
     hxm->connectLeg(index, i + 1, i + 2, i + 3);
   }
 }
