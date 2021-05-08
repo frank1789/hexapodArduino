5410369267957ea1cdf0dd54e569efecae26819f
Francesco Argentieri <francesco.argentieri89@gmail.com>
2021-05-08 15:06:37 +0200

    Fix test function

--- mainservo/servotestfunction.cpp
+++ mainservo/servotestfunction.cpp
@@ -2,18 +2,16 @@
 
 #include <Arduino.h>
 
-void testservo(Leg& legs) {
-  for (int i = 0; i < 6; i++) {
-    legs["coxa"]->setServoAngle(75);
+void testservo(Leg& leg) {
+    leg["coxa"]->setServoAngle(75);
     delay(500);
-    legs["coxa"]->setServoAngle(90);
+    leg["coxa"]->setServoAngle(90);
     delay(500);
-    legs["femur"]->setServoAngle(120);
+    leg["femur"]->setServoAngle(120);
     delay(500);
-    legs["femur"]->setServoAngle(90);
+    leg["femur"]->setServoAngle(90);
     delay(500);
-    legs["tibia"]->setServoAngle(120);
+    leg["tibia"]->setServoAngle(120);
     delay(500);
-    legs["tibia"]->setServoAngle(90);
-  }
+    leg["tibia"]->setServoAngle(90);
 }
