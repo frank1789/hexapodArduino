85da6a812be37e9267706fadcef39ba4e27bd1e4
Francesco Argentieri <francesco.argentieri89@gmail.com>
2021-05-08 16:44:35 +0200

    Test one servo

--- mainservo/mainservo.ino
+++ mainservo/mainservo.ino
@@ -13,32 +13,45 @@ static const char *legs[] = {
 void setup() {
   Serial.begin(115200);
   Serial.print("initialize legs connected to pin\n");
-  for (int i = 0; i < kPins; i += 3) {
-    int index = i / 3;
-    int pin_coxa = i + 1;
-    int pin_femur = i + 2;
-    int pin_tibia = i + 3;
-    auto msg = String(i) + String(" ");
-    msg += "setup leg: ";
-    msg += String("\"") + legs[index] + String("\"") + String("\t");
-    msg += "assign pins: ";
-    msg += "coxa: ";
-    msg += String(pin_coxa) + "\t";
-    msg += "femur: ";
-    msg += String(pin_femur) + "\t";
-    msg += "tibia: ";
-    msg += String(pin_tibia);
-    Serial.println(msg);
-    hxm.connectLeg(index, pin_coxa, pin_femur, pin_tibia);
-  }
+  int i = 0;
+  // for (int i = 0; i < kPins; i += 3) {
+  int index = i / 3;
+  int pin_coxa = i + 1;
+  int pin_femur = i + 2;
+  int pin_tibia = i + 3;
+  auto msg = String(i) + String(" ");
+  msg += "setup leg: ";
+  msg += String("\"") + legs[index] + String("\"") + String("\t");
+  msg += "assign pins: ";
+  msg += "coxa: ";
+  msg += String(pin_coxa) + "\t";
+  msg += "femur: ";
+  msg += String(pin_femur) + "\t";
+  msg += "tibia: ";
+  msg += String(pin_tibia);
+  Serial.println(msg);
+  hxm.connectLeg(index, pin_coxa, pin_femur, pin_tibia);
+  // }
 }
 
 void loop() {
-  for (int i = 0; i < kNumberLegs; i++) {
-    auto name_leg = legs[i];
-    auto tleg = hxm.getLeg(name_leg);
-    Serial.print("test leg: ");
-    Serial.println(name_leg);
-    testservo(tleg);
+  // for (int i = 0; i < kNumberLegs; i++) {
+  // auto name_leg = legs[i];
+  // auto tleg = hxm.getLeg(name_leg);
+  //   Serial.print("test leg: ");
+  //   Serial.println(name_leg);
+  //   testservo(tleg);
+  // }
+
+  auto leg = hxm.getLeg(legs[1]);
+  for (int i = 1; i < 178; i++) {
+    auto str = String("testLeg, for angle: ") + String(i);
+    Serial.println(str);
+    leg.getComponentLeg("coxa").setServoAngle(i);
+    delay(500);
+    leg.getComponentLeg("femur").setServoAngle(i);
+    delay(500);
+    leg.getComponentLeg("tibia").setServoAngle(i);
+    delay(500);
   }
 }
