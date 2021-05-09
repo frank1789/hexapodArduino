8d36bfa3720b3f50fc272fc1384e53def848193a
Francesco Argentieri <francesco.argentieri89@gmail.com>
2021-05-09 11:53:10 +0200

    Add function for i2c communication control with servo

--- mainservo/servomanager.h
+++ mainservo/servomanager.h
@@ -1,7 +1,21 @@
 #ifndef SERVO_MANAGER_H
 #define SERVO_MANAGER_H
 
-#include <Servo.h>
+#include <Wire.h>
+
+
+// I2C functions
+void I2C_SERVOSET(unsigned char servo_num,unsigned int servo_pos);
+void I2C_SERVOREVERSE(unsigned char servo_num,unsigned char servo_dir);
+void I2C_SERVOOFFSET(unsigned char servo_num,int value);
+void I2C_SERVOSPEED(unsigned char value);
+void I2C_SERVONUTRALSET(unsigned char servo_num,unsigned int servo_pos);
+void I2C_SERVOMIN(unsigned char servo_num,unsigned int servo_pos);
+void I2C_SERVOMAX(unsigned char servo_num,unsigned int servo_pos);
+char I2C_SERVOEND(void);
+int I2C_SERVOGET(int servo_num);
+int I2C_SERVOGETOFFSET(int servo_num);
+
 
 class ServoManager {
  public:
@@ -28,7 +42,7 @@ class ServoManager {
   void resetServo();
 
  private:
-  Servo servo_;
+  unsigned char pin_;
 };
 
 #endif  // SERVO_MANAGER_H
