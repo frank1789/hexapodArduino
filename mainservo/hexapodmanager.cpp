33097158036ca4c5871de5fbda99d254b5f40131
Francesco Argentieri <francesco.argentieri89@gmail.com>
2021-05-23 18:45:58 +0200

    Remove pointers

--- mainservo/hexapodmanager.cpp
+++ mainservo/hexapodmanager.cpp
@@ -19,20 +19,9 @@
 HexapodManger::HexapodManger() {
   // assemble pins and legs to correctly initialize the robot
   Serial.print("initialize legs connected to pin\n");
-  for (int i = 0; i < kPins; i += 3) {
-    int index = i / 3;
-    int pin_coxa = i + 1;
-    int pin_femur = i + 2;
-    int pin_tibia = i + 3;
-    auto msg = String(i) + String(" ");
-    msg += "setup leg, assign pins: coxa: ";
-    msg += String(pin_coxa) + "\t";
-    msg += "femur: ";
-    msg += String(pin_femur) + "\t";
-    msg += "tibia: ";
-    msg += String(pin_tibia);
-    Serial.println(msg);
-    this->connectLeg(index, pin_coxa, pin_femur, pin_tibia);
+  for (int i = 0; i < kPins; i++) {
+    auto pin_index = i + 1;
+    servos_[i] = ServoManager((unsigned char)pin_index);
   }
 
   /*
@@ -42,48 +31,6 @@ HexapodManger::HexapodManger() {
    */
 }
 
-HexapodManger::~HexapodManger() {
-  for (int i = 0; i < kNumberLegs; i++) {
-    DELETE(legs_[i])
-  }
-}
+HexapodManger::~HexapodManger() {}
 
-void HexapodManger::connectLeg(int index, int pin_coxa, int pin_femur,
-                               int pin_tibia) {
-  legs_[index] = new Leg(pin_coxa, pin_femur, pin_tibia);
-}
-
-Leg HexapodManger::getLeg(const char* leg_position) {
-  Leg leg;
-  if (leg_position == kLeftFrontLeg) {
-    leg = *legs_[0];
-  }
-  if (leg_position == kLeftMidLeg) {
-    leg = *legs_[1];
-  }
-  if (leg_position == kLeftBackLeg) {
-    leg = *legs_[2];
-  }
-  if (leg_position == kRightFrontLeg) {
-    leg = *legs_[3];
-  }
-  if (leg_position == kRightMidLeg) {
-    leg = *legs_[4];
-  }
-  if (leg_position == kRightBackLeg) {
-    leg = *legs_[5];
-  }
-
-  return leg;
-}
-
-void HexapodManger::getServos(ServoManager p[]) {
-  static const char* legs[] = {kComponentLegCoxa, kComponentLegFemur,
-                        kComponentLegTibia};
-  for (auto i = 0; i < kNumberLegs; i++) {
-    for (auto j = 0; j < 3; j++) {
-      p[i + j] = legs_[i]->getComponentLeg(legs[j]);
-      Serial.println("index: "+String(i*j)+ "pin: "+String(p[i+j].getPin()));
-    }
-  }
-}
+void HexapodManger::getServos(ServoManager p[]) { p = servos_; }
--- mainservo/hexapodmanager.h
+++ mainservo/hexapodmanager.h
@@ -17,30 +17,11 @@ class HexapodManger {
    */
   ~HexapodManger();
 
-  /**
-   * @brief ConnectLeg builds each leg separately and assigns correct pins to
-   * servo motors
-   *
-   * @param[in] index integer
-   * @param[in] pin_coxa integer represents the pin
-   * @param[in] pin_femur integer represents the pin
-   * @param[in] pin_tibia integer represents the pin
-   */
-  void connectLeg(int index, int pin_coxa, int pin_femur, int pin_tibia);
-
-  /**
-   * @brief Get the Leg object
-   *
-   * @param[in] leg_position string leg name's joint
-   * @return Leg
-   */
-  Leg getLeg(const char* leg_position);
 
   void getServos(ServoManager p[]);
 
  private:
-  typedef Leg* legptr_t;
-  legptr_t legs_[kNumberLegs]{};
+  ServoManager servos_[kPins];
 };
 
 #endif  // __HEXAPOD_MANAGER_H__
